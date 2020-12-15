c692f2965855b70496a8b580c32fc25514f75f16
Nicolas Marsal <nicolas.marsal@gmail.com>
2020-12-15 11:11:20 +0100

    ajout d'un attribut 'type' au comp-button

--- projects/concord/src/app/components/comp-button/comp-button.component.ts
+++ projects/concord/src/app/components/comp-button/comp-button.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Attribute, ChangeDetectionStrategy, Component } from "@angular/core";
+
+export type ButtonType = "validation" | "cancelation" | "configuration";
 
 @Component({
-  selector: 'app-comp-button',
-  templateUrl: './comp-button.component.html',
-  styleUrls: ['./comp-button.component.scss']
+  selector: "app-comp-button",
+  templateUrl: "./comp-button.component.html",
+  styleUrls: ["./comp-button.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CompButtonComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class CompButtonComponent {
+  constructor(@Attribute("type") public type: ButtonType = "validation") {}
 }
